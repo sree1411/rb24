14deb6e18dd94d7cd7b783d9839db07a17fb98cc
sree1411 <sreekanthsree141122@gmail.com>
2024-03-30 20:08:47 +0530

    counter app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [count, setCount] = useState(0);
+
+  function increment() {
+    setCount(count + 1);
+  }
+  
+  // function reset() {
+  //   setCount(0); this is normal function below one is the arrow function
+  // }
+  const reset = ()=>{
+    setCount(0);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1> {count} </h1>
+      <button onClick={increment}> + </button>
+      <button onClick={()=>{setCount(count - 1)}}> - </button>
+      <button onClick={reset}> Reset </button>
+    </>
   );
 }
-
 export default App;
+
+// dintlo two types of function rasanu
