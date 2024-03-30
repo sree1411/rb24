9784f55a45c682596867fb9996e7529191279865
sree1411 <sreekanthsree141122@gmail.com>
2024-03-30 19:51:12 +0530

    TEST

--- src/App.js
+++ src/App.js
@@ -1,24 +1,72 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [records, setRecords]= useState([])
+  const [users, setUsers]= useState({
+    firstname:"",
+    lastname:"",
+    dob:""
+  })
+
+  
+
+  function handleInput(e){
+    setUsers({...users, [e.target.name]:e.target.value})
+  }
+   
+ function addUser(e){
+   e.preventDefault()
+   setRecords([...records, users])
+   setUsers({
+    firstname:"",
+    lastname:"",
+    dob:""
+   })
+ }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+       <form>
+         <input type="text" onChange={handleInput} name='firstname'  value={users.firstname} placeholder='enter the firstname'  /> 
+         <input type="text" onChange={handleInput} name='lastname'  value={users.lastname}     placeholder='enter the lastaname'  /> 
+         <input type="text" onChange={handleInput} name='dob'  value={users.dob}   placeholder='enter the dob'  /> 
+          <button onClick={addUser} > Add user </button>
+       </form>
+       
+
+            <table class="table">
+            <thead>
+              <tr>
+                <th scope="col">First</th>
+                <th scope="col">Last</th>
+                <th scope="col">DOB</th>
+              </tr>
+            </thead>
+            <tbody>
+
+            {
+              records.map((person)=>{
+                return <tr>
+                <td>{person.firstname}</td>
+                <td>{person.lastname}</td>
+                <td>{person.dob}</td>
+              </tr>
+              })
+            }
+
+              
+            
+              
+            </tbody>
+</table>
+
+
+
+ 
+
+
+    </>
   );
 }
 
