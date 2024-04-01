4d410e9e45f342104832c7ed37345b1df09a3132
sree1411 <sreekanthsree141122@gmail.com>
2024-04-01 09:55:29 +0530

    formik example

--- src/App.js
+++ src/App.js
@@ -1,24 +1,74 @@
-import logo from './logo.svg';
+import { useFormik } from 'formik';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+
+  const [students, setStudents] = useState([{
+    firstName:"sree",
+    lastName:"kanth"
+  }])
+
+
+
+    const formik = useFormik({
+      initialValues: {
+         firstName: '',
+         lastName: ''
+      },
+      onSubmit: values => {
+        setStudents([...students, values]);
+        formik.resetForm()
+      },
+    });
+ 
+ 
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+     <>
+     <form onSubmit={formik.handleSubmit}>
+        <label htmlFor="firstName"> First Name </label>
+        <input type="text" name="firstName" id="firstName" onChange={formik.handleChange}
+         value={formik.values.firstName} />  <br/>
+        <label htmlFor="lastName"> Last Name </label>
+        <input type="text" name="lastName" id="lastName" onChange={formik.handleChange}
+         value={formik.values.lastName}/> <br/>
+        <button type="submit"> Sign Up </button>
+     </form>
+
+     <table  className="table">
+  <thead>
+    <tr>
+      <th scope="col">First</th>
+      <th scope="col">Last</th>
+
+    </tr>
+  </thead>
+  <tbody>
+    {
+      students.map((student)=>{
+        return <>
+        <tr>
+          <td>{student.firstName}</td>
+          <td>{student.lastName}</td>
+       
+        </tr>
+        
+        </>
+      })
+    }
+   
+   
+    
+  </tbody>
+</table>
+
+
+
+
+
+     </>
   );
 }
 
