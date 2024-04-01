0dd71ac8bc1b6a488d04b5a0cd737334c65ce099
sree1411 <sreekanthsree141122@gmail.com>
2024-04-01 10:09:46 +0530

    like and dislike component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  
+     const [like, setlike] = useState(0)
+     const [dislike, setDislike] = useState(0)
+
+     const likeBtn =()=>{
+      setlike(like + 1)
+     }
+
+     const dislikeBtn =()=>{
+       setDislike(dislike + 1)
+     }
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+     <>
+       <span> {like}</span>   
+       <span> {dislike}</span>  <br/>
+     <button onClick={likeBtn} > Like  </button>
+     <button onClick={dislikeBtn} > DisLike </button>
+     
+     </>
   );
 }
 
