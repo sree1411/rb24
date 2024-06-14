553d0a51d2e922398c27b12278b4030ec24a1f83
sree1411 <sreekanthsree141122@gmail.com>
2024-06-14 21:06:55 +0530

    removed unnrssay files

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import Navbar from "./components/navbar/Navbar";
-import ContactInformation from "./components/contacts/ContactInformation";
 function App() {
   return (
     <>
      
      <Routes>
         <Route path="/" element={<Navbar/>} />
-        <Route path="/contact" element={<ContactInformation />} />
       </Routes>
       
     </>
