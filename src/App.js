85596500cefc5fe6a1b0ee9d66957eabb53f69fa
sree1411 <sreekanthsree141122@gmail.com>
2024-06-29 10:36:40 +0530

    child to parenet

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import ChildComponent from "./components/ChildComponent";
 
 function App() {
+  const [count, setCount] = useState(0);
+
+  function updateValues(action) {
+    if (action === "increment") {
+      setCount(count + 1);
+    }
+    if (action === "decrement") {
+      if (count > 0) {
+        setCount(count - 1);
+      }
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ChildComponent count={count} updateValues={updateValues} />
     </div>
   );
 }
---
+++ src/components/ChildComponent.jsx
@@ -0,0 +1,24 @@
+
+
+function ChildComponent ({count, updateValues}){
+    
+    // const {count, updateValues} = props
+
+     function incBtn(){
+        updateValues("increment")
+     }
+
+     function decBtn(){
+        updateValues("decrement")
+     }
+
+
+    return(
+        <>
+        <h1> child component :{count}</h1>
+        <button onClick={incBtn}> increment </button>
+        <button onClick={decBtn}> decrement </button>
+        </>
+    )
+}
+export default ChildComponent;
