feecfa7c7e448f10241f53b489dcfe3ac49a4fd3
sree1411 <sreekanthsree141122@gmail.com>
2024-06-30 10:57:48 +0530

    redux api calling RTK

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import Counter from "./components/counter/Counter";
 import Countries from "./components/country/Countries";
 import TodoList from "./components/todoList/TodoList";
+import Navbar from "./components/navbar/Navbar";
 
 function App() {
    return(
     <>
+
+
+      <Navbar/>
+
+      <Routes>
+         <Route path="/counter" element={<Counter/>}/>
+         <Route path="/todolist" element={<TodoList/>}/>
+         <Route path="/countries" element={<Countries/>}/>
+      </Routes>
     
-       <Counter/>
-       <TodoList/>
-       <Countries/>
+      
+     
     
     </>
    )
