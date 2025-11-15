cd6a5dd378e6fd7eabcd16c2bfa73d5320b7a9ef
sreekanth <sreekanth>
2025-11-15 07:38:26 +0530

    Revert "Merge pull request #2 from sree1411/Sort_method_api_call"
    This reverts commit ece3642f1eace8dade139ca6038184c34bb64d8a, reversing
    changes made to 36ecf3658a4cfb889f6cf6717105e649dd768d11.

--- src/App.js
+++ src/App.js
@@ -1,83 +1,7 @@
-import { useEffect, useState } from 'react';
-import axios from 'axios';
+import logo from './logo.svg';
 import './App.css';
 
 function App() {
-
-//here we created the sort method by using the button 
-// when we click the button it will work according to the function 
-
-   const[countries, setCountries] = useState([])
-
-
-    useEffect(()=>{
-     axios.get("https://restcountries.com/v3.1/all").then((res)=>{
-      setCountries(res.data)
-     }).catch((error)=>console.log(error.message))
-    },[])
-
-
-   const regionBtnAtoZ = ()=>{
-     countries.sort((a,b)=>{
-      
-      const nameA = a.region.toLowerCase()
-      const nameB = b.region.toLowerCase()
-
-      if(nameA > nameB){
-        return +1
-      } else {
-        return -1
-      }
-     })
-     setCountries([...countries])
-   }
-
-   const regionBtnZtoA = ()=>{
-    countries.sort((a,b)=>{
-     
-     const nameA = a.region.toLowerCase()
-     const nameB = b.region.toLowerCase()
-
-     if(nameA > nameB){
-       return -1
-     } else {
-       return +1
-     }
-    })
-    setCountries([...countries])
-  }
-
-
-const poputionBtnLow =()=>{
-  countries.sort((a,b)=>{
-      
-    const nameA = a.population 
-    const nameB = b.population 
-
-    if(nameA > nameB){
-      return +1
-    } else {
-      return -1
-    }
-   })
-   setCountries([...countries])
-}
-
-const poputionBtnHigh =()=>{
-  countries.sort((a,b)=>{
-      
-    const nameA = a.population 
-    const nameB = b.population 
-
-    if(nameA > nameB){
-      return -1
-    } else {
-      return 1
-    }
-   })
-   setCountries([...countries])
-}
-
   return (
     <div className="App">
       <header className="App-header">
@@ -99,6 +23,3 @@ const poputionBtnHigh =()=>{
 }
 
 export default App;
-
-
-
